09e7f30ad37556ae6fd0f2ed143c20eea7a36be9
YaakovHatam <kobieshka@gmail.com>
2020-11-22 20:22:29 +0200

    Twitter analyze project init

---
+++ index.js
@@ -0,0 +1,18 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'TjWtd8Z8snbMnUKUqsErSX2PN',
+    consumer_secret: 'DHxB3cVQvFLYJbRUzMYyx5WDOHxS0BiUSkNmLqjpqFJnFMlP8s',
+    access_token_key: '326120564-uM3tfJy8nZsVM74y6yOfZg7BLhX99NXWhT0Qq4SH',
+    access_token_secret: 'PgwIg21PqgEesRUptneBDKsIMEOcS1m5fXguk6bn9OZBg'
+});
+
+var params = { q: 'ברק רביד' };
+client.get('users/search', params, function (error, tweets, response) {
+    if (!error) {
+        const data = tweets.filter(acc => acc.verified === true).map(acc => acc.screen_name);
+        console.log(data);
+        // screen_name
+        // verified
+    }
+});
